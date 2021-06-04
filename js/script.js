4b6bc882f84598ce8862ce3744ceb725c22e2ee2
soondooboo97 <82435741+soondooboo97@users.noreply.github.com>
2021-06-04 14:37:11 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $('.section1 .main-visual-img').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+    });
+    
+
+    
+    
+});
